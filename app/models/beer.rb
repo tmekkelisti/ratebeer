9c26d29daa883890d05d21f56259dde9acafb8fb
tme <theme87@gmail.com>
2015-01-19 16:12:53 +0200

    1, 2, 3 done

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,3 +1,13 @@
 class Beer < ActiveRecord::Base
   belongs_to :brewery
+  has_many :ratings
+
+  def average_rating
+    ratingit = self.ratings
+    scoret = 0
+    scoret << ratingit.each do |beer|
+      puts beer.score
+    end
+  end
+
 end
---
+++ app/models/rating.rb
@@ -0,0 +1,8 @@
+class Rating < ActiveRecord::Base
+  belongs_to :beer
+
+  def to_s
+    return self.beer.name + " " + self.score.to_s
+  end
+
+end
