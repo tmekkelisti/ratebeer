7c0e7ff5568f8ab1c30e832d7242f836bfa1eb0e
Teemu Y <theme87@gmail.com>
2015-01-28 02:45:15 +0200

    vko3 1-3 done

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -9,23 +9,18 @@ class RatingsController < ApplicationController
     @beers = Beer.all
   end
 
-  def create
-    Rating.create params.require(:rating).permit(:score, :beer_id)
-    redirect_to ratings_path
-  end
-
   def destroy
     rating = Rating.find(params[:id])
     rating.delete
-    redirect_to ratings_path
+    redirect_to :back
   end
 
   def create
     rating = Rating.create params.require(:rating).permit(:score, :beer_id)
 
-    session[:last_rating] = "#{rating.beer.name} #{rating.score} points"
+    current_user.ratings << rating
 
-    redirect_to ratings_path
+    redirect_to current_user
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
   # GET /users/1
   # GET /users/1.json
   def show
-    @ratings = @user.ratings.all
+    #@user = @user
   end
 
   # GET /users/new
